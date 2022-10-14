a2270c037929c0aa929715e0937c250b818209bd
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-10-14 19:10:31 +0200

    iteration 3 finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/movie.model.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req,res,next) => {
+    Movies.find()
+    
+    .then((response) => {
+        console.log(response)
+        res.render("movies.hbs", {
+            listMovies : response
+        })
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
+})
 
 module.exports = router;
